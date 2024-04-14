2f54f6915f505d5c05226dd5cac4445e9edd4857
Manuel L_ <50886923+mlrcloud@users.noreply.github.com>
2024-04-14 12:49:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 # Write directly to the app
 st.title("Example Streamlit App :cup_with_straw:")
 st.write(
