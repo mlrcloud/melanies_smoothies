de40fe23950738887c20f51bb78d148672f1e136
Manuel L_ <50886923+mlrcloud@users.noreply.github.com>
2024-04-15 10:19:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
@@ -37,7 +33,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
